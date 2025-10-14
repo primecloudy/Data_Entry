7fe54aab3a97503b34191975f44cfe93a1987e59
primecloudy <cld004.primeedge@gmail.com>
2025-10-14 16:59:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -87,5 +87,5 @@ app.delete("/api/users/:username", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log(`✅ Server running on https://entry.primeedge.co.in:${PORT}`);
 });
